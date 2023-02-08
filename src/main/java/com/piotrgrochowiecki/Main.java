30780bcaf02b185c05cf66c63b31c632a11f4352
Piotr Grochowiecki <42697061+piotrgrochowiecki@users.noreply.github.com>
2023-02-08 15:06:24 +0100

    Implementation of interface

--- src/main/java/com/piotrgrochowiecki/Main.java
+++ src/main/java/com/piotrgrochowiecki/Main.java
@@ -5,5 +5,15 @@ public class Main {
     public static void main(String[] args) {
         Greeting greeting = new HelloGreeting();
         greeting.sayHello();
+
+        Greeting greeting2 = new Greeting() {
+            @Override
+            public void sayHello() {
+                System.out.println("Hello World 2");
+            }
+        };
+
+        greeting2.sayHello();
+
+        }
     }
-}
