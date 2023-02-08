be733e1b187f139412046a6927a6bbff93307b39
Piotr Grochowiecki <42697061+piotrgrochowiecki@users.noreply.github.com>
2023-02-08 15:16:51 +0100

    Using already provided functional interface IntBinaryOperator

--- src/main/java/com/piotrgrochowiecki/Calculator.java
+++
@@ -1,8 +0,0 @@
-package com.piotrgrochowiecki;
-
-@FunctionalInterface
-public interface Calculator {
-
-    int calculates(int x, int y);
-
-}
--- src/main/java/com/piotrgrochowiecki/Main.java
+++ src/main/java/com/piotrgrochowiecki/Main.java
@@ -1,6 +1,7 @@
 package com.piotrgrochowiecki;
 
 import java.util.Random;
+import java.util.function.IntBinaryOperator;
 
 public class Main {
 
@@ -21,13 +22,13 @@ public class Main {
         Greeting greeting3 = () -> System.out.println("Hello World 3");
         greeting3.sayHello();
 
-        Calculator calculator = (int x, int y) -> {
+        IntBinaryOperator calculator = (int x, int y) -> {
             Random random = new Random();
             int randomNumber = random.nextInt(50);
             return x * y + randomNumber;
         };
 
-        System.out.println(calculator.calculates(1,2));
+        System.out.println(calculator.applyAsInt(1,2));
 
         }
     }
