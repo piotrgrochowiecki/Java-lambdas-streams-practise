67a6f086747f977a2ff0228f79889fa6828ea4a5
Piotr Grochowiecki <42697061+piotrgrochowiecki@users.noreply.github.com>
2023-02-09 16:01:50 +0100

    Beginning of stream exercise

---
+++ src/main/java/com/piotrgrochowiecki/PlanetUtils.java
@@ -0,0 +1,25 @@
+package com.piotrgrochowiecki;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class PlanetUtils {
+
+    public List<Planet> getPlanetsBeginningWithM(List<Planet> planets) {
+        return null;
+    }
+
+    public List<Planet> getPlanetsWithRings(List<Planet> planets) {
+        return null;
+    }
+
+    public List<Planet> getPlanetsWithMoreThanThreeMoons(List<Planet> planets) {
+        return null;
+    }
+
+    public List<Planet> getPlanetsWithDensityGreaterThanFive(List<Planet> planets) {
+        return null;
+    }
+
+
+}
